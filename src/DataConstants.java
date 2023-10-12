8d7439f5431aaaa2b56711e7e99b0bd7b1bb0613
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2023-10-12 14:03:07 -0400

    closing #26 #28 #21 #22 and starting #27

---
+++ src/DataConstants.java
@@ -0,0 +1,11 @@
+package src;
+
+public class DataConstants {
+    public static final String USER_ID = "id";
+    public static final String USER_USERNAME = "username";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_EMAIL = "email";
+
+    public static final String PROJECT_ID = "id";
+    public static final String PROJECT_OWNER = "owner";
+}
---
+++ src/DataLoader.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class DataLoader {
+    public static ArrayList<User> loadUsers() {
+        ArrayList<User> users = new ArrayList<User>();
+        return users;
+    }
+
+    public static ArrayList<Project> loadProjects() {
+        ArrayList<Project> projects = new ArrayList<Project>();
+        return projects;
+    }
+}
