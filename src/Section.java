ebbb7372c6c1a7ef86a7d4e4ccfdfb5d8d4dd427
Gavinorme <Gorme@email.sc.edu>
2023-10-17 14:34:47 -0400

    working on #7

--- src/Section.java
+++ src/Section.java
@@ -3,29 +3,9 @@ package src;
 import java.util.ArrayList;
 
 public class Section {
+    public String id;
     public String title;
     private ArrayList<Task> tasks;
-
-<<<<<<< HEAD
-    public Section sectionTitle(String title){
-        this.title = title;
-        this.id = id;
-        ArrayList<Task> tasks = new ArrayList<Task>();
-        return sectionTitle(title);
-    }
-
-    public boolean addTask(Task task){
-        Task newTask = new Task(title, description, 0, category);
-        return true;
-    }
-
-    public boolean removeTask(Task task){
-        tasks.remove(task);
-=======
-    public Section(String title) {
-        this.title = title;
-        this.tasks = new ArrayList<Task>();
-    }
 
     public Section(String title, ArrayList<Task> tasks) {
         this.title = title;
@@ -39,7 +19,6 @@ public class Section {
 
     public boolean removeTask(Task task) {
         this.tasks.remove(task);
->>>>>>> 9c2772e9a2a036a41693f5525831e8685191c05d
         return true;
     }
 }
