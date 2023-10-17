93d3d6a70b6efee5dec659fead5d24bc0f7c1c42
Alexis-Hill6 <aah6@email.sc.edu>
2023-10-17 14:26:25 -0400

    data writer filled out

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -59,7 +59,38 @@ public class DataWriter extends DataConstants {
         JSONObject projectDetails = new JSONObject();
         projectDetails.put(PROJECT_ID, project.getProjectID());
         projectDetails.put(PROJECT_TITLE, project.getTitle());
-        projectDetails.put()
+        projectDetails.put(PROJECT_START, project.getStart());
+        projectDetails.put(PROJECT_END, project.getEnd());
+        projectDetails.put(PROJECT_SECTIONS, project.getSections());
+        projectDetails.put(PROJECT_SECTIONS_ID, project.getSectionID().toString());
+        projectDetails.put(PROJECT_SECTIONS_TITLE, project.getSectionTitle());
+        projectDetails.put(PROJECT_SECTIONS_DESCRIPTION, project.getDescription());
+        projectDetails.put(PROJECT_SECTIONS_TYPE, project.getType());
+        projectDetails.put(PROJECT_SECTIONS_ASSIGNED_USERS, project.getAssignedUsers());
+        projectDetails.put(PROJECT_SECTIONS_COMPLETION, project.getCompletion());
+        projectDetails.put(PROJECT_SECTIONS_PRIORITY, project.getPriority());
+        projectDetails.put(PROJECT_SECTIONS_CATEGORY, project.getCategory());
+        projectDetails.put(PROJECT_SECTIONS_COMMENTS, project.getSectionComments());
+        projectDetails.put(PROJECT_SECTIONS_COMMENTS_ID, project.getSectionCommentsID());
+        projectDetails.put(PROJECT_SECTIONS_COMMENTS_CONTENT, project.getSectionCommentContent());
+        projectDetails.put(PROJECT_SECTIONS_COMMENTS_DATE, project.getSectionCommentDate());
+        projectDetails.put(PROJECT_SECTIONS_COMMENTS_USER, project.getSectionCommentUser());
+        projectDetails.put(PROJECT_SECTIONS_COMMENTS_COMMENTS, project.getSectionCommentsComment());
+        projectDetails.put(PROJECT_SECTIONS_CHANGELOG, project.getSectionChange());
         projectDetails.put(PROJECT_USERS, project.getProjectUser());
+        projectDetails.put(PROJECT_COMMENTS, project.getProjectComments());
+        projectDetails.put(PROJECT_COMMENTS_ID, project.getProjectCommentsID());
+        projectDetails.put(PROJECT_COMMENTS_CONTENT, project.getProjectContent());
+        projectDetails.put(PROJECT_COMMENTS_DATE, project.getProjectCommentDate());
+        projectDetails.put(PROJECT_COMMENTS_COMMENTS, project.getProjectCommentsComments());
+        projectDetails.put(PROJECT_COMMENTS_USER, project.getProjectCommentUser());
+        projectDetails.put(PROJECT_BACKLOG, project.getProjectBacklog());
+        projectDetails.put(PROJECT_BACKLOG_ID, project.getBacklogID());
+        projectDetails.put(PROJECT_BACKLOG_TASK, project.getBacklogasks());
+        projectDetails.put(PROJECT_BACKLOG_PREVIOUS_SECTION, project.getBacklogPrev());
+        projectDetails.put(PROJECT_BACKLOG_NEXT_SECTION, project.getBacklogNextSection());
+        projectDetails.put(PROJECT_BACKLOG_DATE, project.getBacklogDate());
+        projectDetails.put(PROJECT_BACKLOG_USER_EDITED, project.getBacklogEdits());
+
     }
 }
