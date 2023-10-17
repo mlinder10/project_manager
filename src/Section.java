9b59c88a92d1027b461a4f4c05198fa8bb069f91
Matt Linder <linder2015@outlook.com>
2023-10-17 14:23:28 -0400

    #34

--- src/Section.java
+++ src/Section.java
@@ -1,22 +1,18 @@
 package src;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class Section {
-    private UUID id;
-    private String title;
+    public String title;
     private ArrayList<Task> tasks;
 
     public Section(String title) {
         this.title = title;
-        this.id = UUID.randomUUID();
         this.tasks = new ArrayList<Task>();
     }
 
-    public Section(String title, UUID id, ArrayList<Task> tasks) {
+    public Section(String title, ArrayList<Task> tasks) {
         this.title = title;
-        this.id = id;
         this.tasks = tasks;
     }
 
