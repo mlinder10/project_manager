9ffdd04a68a13b8a40875c8198ad5994af86c5c3
Matt Linder <linder2015@outlook.com>
2023-10-17 14:03:47 -0400

    #33

--- src/Project.java
+++ src/Project.java
@@ -1,48 +1,43 @@
 package src;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.UUID;
 
 public class Project {
     UUID id;
     User owner;
     String title;
-    Date start;
-    Date end;
     ArrayList<Comment> comments;
     ArrayList<User> users;
     ArrayList<Section> sections;
     ArrayList<Change> backlog;
 
-    public Project(String title, User owner, Date startDate, Date endDate){
+    public Project(String title, User owner) {
 
     }
-    public Project(UUID id, String title, User owner, Date startDate, Date endDate){
-        
+
+    public Project(UUID id, String title, User owner) {
+
     }
 
-    public boolean editProject(String title){
+    public boolean editProject(String title) {
         return true;
     }
 
-    public boolean addSection(Section section){
+    public boolean addSection(Section section) {
         return true;
     }
 
-    public boolean removeSection(Section section){
+    public boolean removeSection(Section section) {
         return true;
     }
 
-    public boolean addUser(User user){
+    public boolean addUser(User user) {
         return true;
     }
 
-    public boolean addComent(Comment comment){
+    public boolean addComent(Comment comment) {
         return true;
     }
-
-
-
 
 }
