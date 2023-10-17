3658dd7fa30115db8e4aa24b3407edd9550967e3
Matt Linder <linder2015@outlook.com>
2023-10-17 14:12:40 -0400

    #5

--- src/Task.java
+++ src/Task.java
@@ -1,12 +1,13 @@
 package src;
+
 /**
  * @author Gavin Orme
  */
 import java.util.ArrayList;
+import java.util.UUID;
 
- public class Task
- {
-    public String id;
+public class Task {
+    public UUID id;
     public String description;
     public String title;
     public String type;
@@ -15,15 +16,32 @@ import java.util.ArrayList;
     public int priority;
     public String category;
     public ArrayList<Comment> comments;
-    public ArrayList<SectionTitle> changeLog;
+    public ArrayList<String> changeLog;
 
-    public Task(String title, String description, int priority, String category)
-    {
-
+    public Task(String title, String description, int priority, String category) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.category = category;
     }
 
-    public boolean addComment(Comment comment)
-    {
+    public Task(UUID id, String title, String description, String type, ArrayList<User> assignedUsers,
+            boolean completion, int priority, String category, ArrayList<Comment> comments,
+            ArrayList<String> changeLog) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.type = type;
+        this.assignedUsers = assignedUsers;
+        this.completion = completion;
+        this.priority = priority;
+        this.category = category;
+        this.comments = comments;
+        this.changeLog = changeLog;
+    }
+
+    public boolean addComment(Comment comment) {
+        this.comments.add(comment);
         return true;
     }
- }
+}
