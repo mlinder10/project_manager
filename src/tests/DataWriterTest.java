02260c6f27146ae88c1c3d3db97d9da336a80bca
Matt Linder <linder2015@outlook.com>
2023-11-02 14:10:30 -0400

    #48

--- src/tests/DataWriterTest.java
+++ src/tests/DataWriterTest.java
@@ -1,10 +1,14 @@
 package src.tests;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 
 
 public class DataWriterTest {
 
-    
+    @Test
+    public void testing() {
+        assertTrue(true);
+    }
 }
