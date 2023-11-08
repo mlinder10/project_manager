821fc3ff208505ab0285a4139650df53cbcec9bf
Gavinorme <Gorme@email.sc.edu>
2023-11-07 21:40:42 -0500

    Project test

--- src/tests/ProjectTest.java
+++ src/tests/ProjectTest.java
@@ -2,9 +2,15 @@ package src.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import src.Comment;
 import src.Project;
+import src.Section;
+import src.User;
+import src.statuses.CreateSectionStatus;
 
 
 public class ProjectTest {
@@ -16,12 +22,18 @@ public class ProjectTest {
 
     @Test 
     public void createSection() {
-        
+        ArrayList<Section> sections = new ArrayList<>();
+        Section section = new Section("TODO");
+        sections.add(section);
+        assertEquals(true, sections.add(section));
     }
 
     @Test 
     public void deleteSection() {
-        
+        ArrayList<Section> sections = new ArrayList<>();
+        Section section = new Section("TODO");
+        sections.add(section);
+        assertEquals(true, sections.remove(section));
     }
 
     @Test 
@@ -31,11 +43,19 @@ public class ProjectTest {
 
     @Test 
     public void createComment() {
-        
+        ArrayList<Comment> comments = new ArrayList<>();
+        User userOne = new User("asmith", "password", "asmith@gmail.com");
+        Comment comment = new Comment("Make impossible burger possible", userOne);
+        comments.add(comment);
+        assertEquals(true, comments.add(comment));
     }
 
     @Test 
     public void deleteComment() {
-        
+        ArrayList<Comment> comments = new ArrayList<>();
+        User userOne = new User("asmith", "password", "asmith@gmail.com");
+        Comment comment = new Comment("Make impossible burger possible", userOne);
+        comments.add(comment);
+        assertEquals(true, comments.remove(comment));
     }
 }
