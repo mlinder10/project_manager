e028f19b70ddf30ba033a2585e9d3f6bc177c9eb
Alexis-Hill6 <aah6@email.sc.edu>
2023-11-07 22:30:47 -0500

    testing

--- src/tests/DataWriterTest.java
+++ src/tests/DataWriterTest.java
@@ -1,14 +1,49 @@
 package src.tests;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.Assertions.*;
 import org.junit.Test;
 
+import org.junit.jupiter.api.BeforeEach;
 
+import src.UserList;
+
+import org.junit.jupiter.api.AfterEach;
+import java.util.ArrayList;
 
 public class DataWriterTest {
 
-    @Test
-    public void testing() {
-        assertTrue(true);
+    private ArrayList<User> users = UserList.getUserList();
+
+    @BeforeEach
+
+    public void setup() {
+        UserList.getUserList().clear();
+        DataWriter.saveUsers();
+
     }
+
+    @AfterEach
+
+    public void tearDown() {
+        UserList.getUserList().clear();
+        DataWriter.saveUsers();
+    }
+
+    @Test
+
+    public void testWritingZeroUsers(){
+        userList = DataLoader.loadUsers();
+        assertEquals(0, userList.size());
+    }
+
+    @Test
+
+    public void testWritingOneUser() {
+        users.add(new User("hill", "12345678"));
+        DataWriter.saveUsers();
+        assertEquals("hill", DataLoader.loadUsers().get(0).username);
+    }
+
+
+
 }
