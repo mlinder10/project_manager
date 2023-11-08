67bec7bd99789f107de5dd8d3f245251bca96d41
Donovan Williams <dw58@email.sc.edu>
2023-11-07 21:40:49 -0500

    project test

--- src/tests/ProjectTest.java
+++ src/tests/ProjectTest.java
@@ -2,16 +2,24 @@ package src.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import src.Project;
+import src.User;
 
 
 public class ProjectTest {
  
+    private String title;
+
     @Test 
     public void editProjectTitle() {
-        
+        User user = new User("atticusmadden", "password", "amadden@gmail.com");
+        Project project = new Project("Electric Missles", user);
+        this.title = "hi";
+        assertEquals("hi", title);
     }
 
     @Test 
@@ -26,7 +34,10 @@ public class ProjectTest {
 
     @Test 
     public void addUser() {
-        
+        ArrayList<User> users = new ArrayList<>();
+        User user = new User("atticusmadden", "password", "amadden@gmail.com");
+        users.add(user);
+        assertEquals(true, users.add(user));
     }
 
     @Test 
