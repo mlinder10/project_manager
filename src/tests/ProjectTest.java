31af8239eece06529ec865c0c47ee1d0330c8f48
Donovan Williams <dw58@email.sc.edu>
2023-11-07 21:23:55 -0500

    project test

---
+++ src/tests/ProjectTest.java
@@ -0,0 +1,41 @@
+package src.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import src.Project;
+
+
+public class ProjectTest {
+ 
+    @Test 
+    public void editProjectTitle() {
+        
+    }
+
+    @Test 
+    public void createSection() {
+        
+    }
+
+    @Test 
+    public void deleteSection() {
+        
+    }
+
+    @Test 
+    public void addUser() {
+        
+    }
+
+    @Test 
+    public void createComment() {
+        
+    }
+
+    @Test 
+    public void deleteComment() {
+        
+    }
+}
